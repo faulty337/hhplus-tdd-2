1c510f1d7ee1d57b10c0999de9d97f2746fae7b1
DESKTOP-A6GU9RJ\fault <faulty337@gmail.com>
2024-06-23 08:27:53 +0900

    Add source files

---
+++ LectureApp/src/main/java/com/hhp/lectureapp/LectureAppApplication.java
@@ -0,0 +1,13 @@
+package com.hhp.lectureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureAppApplication.class, args);
+	}
+
+}
---
+++ LectureApp/src/test/java/com/hhp/lectureapp/LectureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhp.lectureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
